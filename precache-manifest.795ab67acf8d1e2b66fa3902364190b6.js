77c733b303a774b90267a2d2266cc688a00a76cf
Zishan Pathan <121816311+zishanPathan@users.noreply.github.com>
2023-03-18 15:09:41 +0530

    Add files via upload

---
+++ precache-manifest.795ab67acf8d1e2b66fa3902364190b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3335a81cba0a2f62f5",
+    "url": "/static/css/main.2cea85e1.chunk.css"
+  },
+  {
+    "revision": "ef3335a81cba0a2f62f5",
+    "url": "/static/js/main.ef3335a8.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "a98502a8490fee943c44",
+    "url": "/static/js/2.a98502a8.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "0aa3d9b759a915f8b6c92782456b1dac",
+    "url": "/index.html"
+  }
+];
